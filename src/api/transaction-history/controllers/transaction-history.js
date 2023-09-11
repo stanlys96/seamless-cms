2f8c11fb0ededcc8cb14823d773c5b2a40e87964
stanlys96 <stanlyskwok@gmail.com>
2023-09-12 00:30:33 +0700

    wlao

--- src/api/transaction-history/controllers/transaction-history.js
+++ src/api/transaction-history/controllers/transaction-history.js
@@ -9,7 +9,7 @@ const { createCoreController } = require("@strapi/strapi").factories;
 require("dotenv").config();
 
 const axiosCustom = axios.default.create({
-  baseURL: "https://bigflip.id/api/v2/disbursement",
+  baseURL: "https://bigflip.id/api/v2",
   headers: {
     Authorization: process.env.FLIP_AUTH,
   },
@@ -21,9 +21,17 @@ module.exports = createCoreController(
     async inquiry(ctx) {
       try {
         const inquire = await axiosCustom.post(
-          "/bank-account-inquiry",
+          "/disbursement/bank-account-inquiry",
           ctx.request.body
         );
+        return inquire.data;
+      } catch (e) {
+        return e.response.data;
+      }
+    },
+    async bankAccounts(ctx) {
+      try {
+        const inquire = await axiosCustom.get("/general/banks");
         return inquire.data;
       } catch (e) {
         return e.response.data;
--- src/api/transaction-history/routes/custom.js
+++ src/api/transaction-history/routes/custom.js
@@ -8,5 +8,13 @@ module.exports = {
         auth: false,
       },
     },
+    {
+      method: "GET",
+      path: "/banks",
+      handler: "transaction-history.bankAccounts",
+      config: {
+        auth: false,
+      },
+    },
   ],
 };
