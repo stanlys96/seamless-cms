d46ca28c9809d7cc8a30df00c99e20eac5ff6fc9
stanlys96 <stanlyskwok@gmail.com>
2023-09-30 19:29:28 +0700

    walao

--- src/helper.js
+++ src/helper.js
@@ -1,56 +1,58 @@
-export const chainData = [
-  {
-    id: 1,
-    chainId: 1,
-    name: "Ethereum",
-    transactionUrl: "https://etherscan.io/tx/",
-  },
-  {
-    id: 2,
-    chainId: 42161,
-    name: "Arbitrum",
-    transactionUrl: "https://arbiscan.io/tx/",
-  },
-  {
-    id: 3,
-    chainId: 137,
-    name: "Polygon",
-    transactionUrl: "https://polygonscan.com/tx/",
-  },
-  {
-    id: 4,
-    chainId: 10,
-    name: "Optimism",
-    transactionUrl: "https://optimistic.etherscan.io/tx/",
-  },
-  {
-    id: 5,
-    chainId: 56,
-    name: "BSC",
-    transactionUrl: "https://bscscan.com/tx/",
-  },
-  {
-    id: 6,
-    chainId: 5,
-    name: "Goerli",
-    transactionUrl: "https://goerli.etherscan.io/tx/",
-  },
-  {
-    id: 7,
-    chainId: 1313161554,
-    name: "Aurora",
-    transactionUrl: "https://explorer.aurora.dev/tx/",
-  },
-  {
-    id: 8,
-    chainId: 84531,
-    name: "Base",
-    transactionUrl: "https://basescan.org/tx/",
-  },
-  {
-    id: 9,
-    chainId: 59114,
-    name: "Linea",
-    transactionUrl: "https://lineascan.build/tx/",
-  },
-];
+module.exports = {
+  chainData: [
+    {
+      id: 1,
+      chainId: 1,
+      name: "Ethereum",
+      transactionUrl: "https://etherscan.io/tx/",
+    },
+    {
+      id: 2,
+      chainId: 42161,
+      name: "Arbitrum",
+      transactionUrl: "https://arbiscan.io/tx/",
+    },
+    {
+      id: 3,
+      chainId: 137,
+      name: "Polygon",
+      transactionUrl: "https://polygonscan.com/tx/",
+    },
+    {
+      id: 4,
+      chainId: 10,
+      name: "Optimism",
+      transactionUrl: "https://optimistic.etherscan.io/tx/",
+    },
+    {
+      id: 5,
+      chainId: 56,
+      name: "BSC",
+      transactionUrl: "https://bscscan.com/tx/",
+    },
+    {
+      id: 6,
+      chainId: 5,
+      name: "Goerli",
+      transactionUrl: "https://goerli.etherscan.io/tx/",
+    },
+    {
+      id: 7,
+      chainId: 1313161554,
+      name: "Aurora",
+      transactionUrl: "https://explorer.aurora.dev/tx/",
+    },
+    {
+      id: 8,
+      chainId: 84531,
+      name: "Base",
+      transactionUrl: "https://basescan.org/tx/",
+    },
+    {
+      id: 9,
+      chainId: 59114,
+      name: "Linea",
+      transactionUrl: "https://lineascan.build/tx/",
+    },
+  ],
+};
