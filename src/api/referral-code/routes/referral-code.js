a1624147eb7b33f322f47602a4f63724a73855b1
Ubuntu <ubuntu@ip-172-31-35-63.ap-southeast-1.compute.internal>
2023-10-18 13:16:42 +0000

    wlao

---
+++ src/api/referral-code/routes/referral-code.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * referral-code router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::referral-code.referral-code');
