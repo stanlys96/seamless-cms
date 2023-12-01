82b035c4b3e1f48c10ec60fa830db46aea8b61e4
stanlys96 <stanlyskwok@gmail.com>
2023-12-01 21:05:05 +0700

    wlao

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@ module.exports = {
                 },
                 {
                   headers: {
-                    "idempotency-key": transactionData.idempotency_key + "xxx",
+                    "idempotency-key": transactionData.idempotency_key,
                   },
                 }
               );
